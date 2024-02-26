7db78a1f6ef078c9edd5084d4aa160fc0ddd6334
dinhnguyencntt <dn1994099@gmail.com>
2024-02-26 11:21:37 +0700

    Add project files.

---
+++ enc_temp_folder/8e9ae010351097ffa3b6a225526a2d1/LTCB_pheptinhcoban.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int tinhtong(int so1, int so2) {
+	return so1 + so2;
+}
+int tinhhieu(int so1, int so2) {
+	return so1 - so2;
+}
+int tinhtich(int so1, int so2) {
+	return so1 * so2;
+}
+int tinhthuong(int so1, int so2) {
+	return so1 / so2;
+}
+int main() {
+	int so1, so2;
+	float ketqua;
+	cout << "chuong trinh tinh cac phep tinh co ban\n";
+	cout << "nhap so thu nhat:";
+	cin >> so1;
+	cout << "nhap so thu hai:";
+	cin >> so2;
+	ketqua = tinhtong(so1, so2);
+	cout << so1 << "+" << so2 << "=" << ketqua << endl;
+	ketqua = tinhhieu(so1, so2);
+	cout << so1 << "-" << so2 << "=" << ketqua << endl;
+	ketqua = tinhtich(so1, so2);
+	cout << so1 << "*" << so2 << "=" << ketqua << endl;
+	ketqua = tinhthuong(so1, so2);
+	cout << so1 << "/" << so2 << "=" << ketqua << endl;
+	if(so2!=0){
+		ketqua= (float)so1 / so2;
+   }else {
+		cout << "khong the chia" << so1 << "cho 0\n";
+	}
+	return 0;
+}
